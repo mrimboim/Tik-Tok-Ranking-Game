513c0b517c7d0105c386075e0c8fe23c5438ed4a
Michael Rimboim <81452346+mrimboim@users.noreply.github.com>
2022-05-19 00:27:13 +0000

    made hearts work

--- public/compare.js
+++ public/compare.js
@@ -7,9 +7,10 @@ for (let i = 0; i < 2; i++) {
   reload.addEventListener("click", function() { reloadVideo(videoElmts[i]) });
   heartButtons[i].classList.add("unloved");
 } // for loop
-
-// hard-code videos for now
-// You will need to get pairs of videos from the server to play the game.
+for (let i = 0; i < 2; i++) {
+  let heart = heartButtons[i];
+  heart.addEventListener("click", function() { heartbreaker(i) });
+} 
 async function getComps() {
   let videos = await sendGetRequest('/getTwoVideos')
   console.log("Line 15:", videos[0].url)
@@ -28,5 +29,32 @@ async function getComps() {
 }
 
 getComps();
+
+function heartbreaker(index){
+  let heart1 = document.getElementById("heart1");
+  let heart2 = document.getElementById("heart2");
+  if(index == 0){
+    heart1.classList.remove("far");
+    heart2.classList.add("far");
+    heart2.classList.add("unloved");
+    heart2.classList.remove("loved");
+    heart1.classList.add("fas");
+    heart1.classList.remove("unloved");
+    heart1.classList.add("loved");
+  }else{
+    
+    heart2.classList.remove("far");
+    heart1.classList.add("far");
+    heart1.classList.add("unloved");
+    heart1.classList.remove("loved");
+    heart2.classList.add("fas");
+    heart2.classList.remove("unloved");
+    heart2.classList.add("loved");
+  
+  }
+}
+// hard-code videos for now
+// You will need to get pairs of videos from the server to play the game.
+
 
 

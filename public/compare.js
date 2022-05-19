4f89e44ca0d6f043cf4fefce5bd1f2e25a4a4e37
Michael Rimboim <81452346+mrimboim@users.noreply.github.com>
2022-05-19 01:00:37 +0000

    moving to part 5

--- public/compare.js
+++ public/compare.js
@@ -1,4 +1,9 @@
 let videoElmts = document.getElementsByClassName("tiktokDiv");
+let selectedVid = 2;
+let choosen = 0;
+let nextButton = document.getElementById("nextButt");
+nextButton.classList.remove("enabledButton");
+nextButton.classList.add("disabledButton");
 
 let reloadButtons = document.getElementsByClassName("reload");
 let heartButtons = document.querySelectorAll("div.heart");
@@ -10,30 +15,45 @@ for (let i = 0; i < 2; i++) {
 for (let i = 0; i < 2; i++) {
   let heart = heartButtons[i];
   heart.addEventListener("click", function() { heartbreaker(i) });
-} 
+}
 async function getComps() {
   let videos = await sendGetRequest('/getTwoVideos')
   console.log("Line 15:", videos[0].url)
 
   const urls = [videos[0].url,
-    videos[1].url];
+  videos[1].url];
 
   for (let i = 0; i < 2; i++) {
     addVideo(urls[i], videoElmts[i]);
   }
   // load the videos after the names are pasted in! 
-  document.getElementById("nickname1").textContent =(videos[0].nickname);
-  document.getElementById("nickname2").textContent =(videos[1].nickname);
-  
+  document.getElementById("nickname1").textContent = (videos[0].nickname);
+  document.getElementById("nickname2").textContent = (videos[1].nickname);
+
   loadTheVideos();
+  
+  nextButton.addEventListener("click", function() { nextClicked(videos) });
 }
 
 getComps();
 
-function heartbreaker(index){
+function nextClicked(videos){
+  if(selectedVid == 2){
+    
+  }else{
+  console.log("choosen video is: ", videos[selectedVid].nickname)
+  }
+}
+
+function heartbreaker(index) {
+  if (selectedVid == 2) {
+    nextButton.classList.add("enabledButton");
+    nextButton.classList.remove("disabledButton");
+  }
   let heart1 = document.getElementById("heart1");
   let heart2 = document.getElementById("heart2");
-  if(index == 0){
+  if (index == 0) {
+    selectedVid = 0;
     heart1.classList.remove("far");
     heart2.classList.add("far");
     heart2.classList.add("unloved");
@@ -41,8 +61,8 @@ function heartbreaker(index){
     heart1.classList.add("fas");
     heart1.classList.remove("unloved");
     heart1.classList.add("loved");
-  }else{
-    
+  } else {
+    selectedVid = 1;
     heart2.classList.remove("far");
     heart1.classList.add("far");
     heart1.classList.add("unloved");
@@ -50,9 +70,9 @@ function heartbreaker(index){
     heart2.classList.add("fas");
     heart2.classList.remove("unloved");
     heart2.classList.add("loved");
-  
   }
 }
+
 // hard-code videos for now
 // You will need to get pairs of videos from the server to play the game.
 
